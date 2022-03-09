39d4c1929e5b29aa42f9a46d83b6771b36c8fc3a
Tiffany Asbell <tasbell26@gmail.com>
2022-03-08 20:12:13 -0600

    7.2

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-prototype-builtins */
 /* eslint-disable prettier/prettier */
 import { Header, Footer, Main, Nav } from "./components";
 import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 
+// for api key
+import dotenv from "dotenv";
+dotenv.config();
+
+// nav bar navigator
 const router = new Navigo("/");
 function render(st) {
   document.querySelector("#root").innerHTML = `
@@ -20,6 +27,7 @@ function render(st) {
 
 // render(state.Home);
 
+// eventlistener for nav bar
 function addEventListener(st) {
   document.querySelectorAll("nav a").forEach((navLink) =>
     navLink.addEventListener("click", (event) => {
@@ -27,13 +35,40 @@ function addEventListener(st) {
       render(state[event.target.title]);
     })
   );
-
+  // toggle for hamburger
   document
     .querySelector(".fa-bars")
     .addEventListener("click", () =>
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
 }
+
+// router hooks function to help with organization and reduce race errors
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=st.%20louis&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
+        )
+        .then((response) => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch((err) => console.log(err));
+    }
+  },
+});
+
 router
   .on({
     "/": () => render(state.Home),
--- store/Home.js
+++ store/Home.js
@@ -1,4 +1,6 @@
+/* eslint-disable prettier/prettier */
 export default {
   header: "My Home Page",
   view: "Home",
+  weather: {},
 };
