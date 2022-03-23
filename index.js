ace7ff84304bb3addcee8a9549c81eb67ff9d3b8
Tiffany Asbell <tasbell26@gmail.com>
2022-03-22 21:11:51 -0500

    yes

--- index.js
+++ index.js
@@ -38,14 +38,42 @@ function addEventListener(st) {
     );
   console.log(st);
   // using state to make sure code only runs on that specific page
+
   if (st.view === "Orders") {
-    console.log("matsinet-st.view:", st.view);
-    // allows you to grab the event and data associated to the event
     document.querySelector("form").addEventListener("submit", (event) => {
       event.preventDefault();
 
       const inputList = event.target.elements;
       console.log("Input Element List", inputList);
+
+      const toppings = [];
+      // Interate over the toppings input group elements
+      for (let input of inputList.toppings) {
+        // If the value of the checked attribute is true then add the value to the toppings array
+        if (input.checked) {
+          toppings.push(input.value);
+        }
+      }
+
+      const requestData = {
+        customer: inputList.customer.value,
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings,
+      };
+      console.log("request Body", requestData);
+
+      axios
+        .post(`${process.env.PIZZA_PLACE_API_URL}`, requestData)
+        .then((response) => {
+          // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          state.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        .catch((error) => {
+          console.log("It puked", error);
+        });
     });
   }
 }
